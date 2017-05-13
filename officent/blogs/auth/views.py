eae299629f13a1ab356cd86ffc55a56d29efe653
wujunkui <wujunkui407@icloud.com>
2017-05-13 15:50:15 +0800

    upload function init

--- officent/blogs/auth/views.py
+++ officent/blogs/auth/views.py
@@ -1,14 +1,18 @@
 # coding:utf-8
+import os
+
 from public import Log
-from flask import redirect
+from flask import redirect, current_app
 from blogs import db
 from blogs.auth import auth
 from blogs.models import Users, UserInfo
 from flask import request, render_template, url_for, flash, abort, session
-from flask.ext.login import login_required, login_user, logout_user
+from flask.ext.login import login_required, login_user, logout_user, current_user
 from .forms import LoginForm, RegistForm
+from werkzeug.utils import secure_filename
 
 LOG = Log()
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -52,6 +56,7 @@ def user_register():
 
     return render_template('register.html', form=form, error=error)
 
+
 @auth.route(r'/info/<name>')
 def user_info(name):
     username = name
@@ -59,8 +64,20 @@ def user_info(name):
     flash(u"Hi {0}".format(username))
     return ""
 
-@auth.route('/upload/<name>',methods=['POST'])
+
+@auth.route('/upload/<name>', methods=['POST'])
 def upload_file(name):
     f = request.files['file_up']
-    f.save()
-    return ''
+    real_filename = secure_filename(f.filename)
+    ext = real_filename.rsplit('.', 1)[1]
+    if not ext in current_app.config['ALLOW_FILES']:
+        LOG.error(real_filename)
+        flash(u'不支持的文件格式')
+        return redirect(url_for('main.index'))
+    save_filepath = os.path.join(current_app.config['UPLOAD_DIR'], current_user.user_name)
+    if not os.path.exists(save_filepath):
+        os.mkdir(save_filepath)
+
+    f.save(os.path.join(save_filepath, real_filename))
+    flash(u'上传文件成功')
+    return redirect(url_for('main.index'))
--- officent/blogs/setting.py
+++ officent/blogs/setting.py
@@ -4,7 +4,13 @@ DEBUG = True
 
 SECRET_KEY = 'this is a secret'
 
+ROOT_PATH = os.path.dirname(os.path.dirname(__file__))
+
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:////' + os.path.dirname(os.path.dirname(__file__)) + '/db/test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:////' + ROOT_PATH + '/db/test.db'
 # SQLALCHEMY_DATABASE_URI = 'mysql://root:root@localhost:3306/devlop'
+
+UPLOAD_DIR = os.path.join(ROOT_PATH,'uploads')
+
+ALLOW_FILES = ['jpg','gif','png','jpeg']
